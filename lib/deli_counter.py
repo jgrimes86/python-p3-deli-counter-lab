4343621ee632e5c9d762a4e11b833ea99c4db072
Nicole Casteel <thepronicole888@gmail.com>
2023-10-23 15:58:37 -0500

    complete lab

---
+++ lib/deli_counter.py
@@ -0,0 +1,21 @@
+katz_deli = []
+def line(customers):
+    if len(customers) == 0:
+        print("The line is currently empty.")
+    else:
+        customers_list = []
+        i = 0 
+        while i < len(customers):
+            message = f'{i+1}. {customers[i]}'
+            customers_list.append(message)
+            i+=1
+        print(f'The line is currently: {" ".join(customers_list)}')
+def take_a_number(customers, name):
+    customers.append(name)
+    print(f'Welcome, {name}. You are number {customers.index(name)+1} in line.')
+def now_serving(customer):
+    if len(customer) > 0:
+        name = customer.pop(0)
+        print(f'Currently serving {name}.')
+    else:
+        print("There is nobody waiting to be served!")
