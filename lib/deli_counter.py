a29b09031f1fb1c56411637ac54e7aefe481da3e
Jim Grimes <jimgrimes86@gmail.com>
2023-10-23 17:03:04 -0400

    completed lab

--- lib/deli_counter.py
+++ lib/deli_counter.py
@@ -1,4 +1,6 @@
+
 katz_deli = []
+
 def line(customers):
     if len(customers) == 0:
         print("The line is currently empty.")
@@ -10,9 +12,11 @@ def line(customers):
             customers_list.append(message)
             i+=1
         print(f'The line is currently: {" ".join(customers_list)}')
+
 def take_a_number(customers, name):
     customers.append(name)
     print(f'Welcome, {name}. You are number {customers.index(name)+1} in line.')
+
 def now_serving(customer):
     if len(customer) > 0:
         name = customer.pop(0)
